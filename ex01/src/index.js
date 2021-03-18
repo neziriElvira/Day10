4b96569a29b50129a1532b775bd127fbbae85bb7
Elvira Neziri <neziiri.elvira@gmail.com>
2021-03-18 09:46:50 +0100

    Added all exercises from Day10

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+
+var myPet = {
+    species: "rabbit",
+    name: "Raby",
+    legs: 4,
+    friends: ["firstBestFriend", "secondBestFriend"]
+};
+
+function myFunction(myObj) {
+    return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
---
+++ ex06/src/index.js
@@ -0,0 +1,21 @@
+// Create lion object here
+
+var lion = {
+    name: "Simba",
+    legs: 4,
+    tails: 1
+};
+
+// End of lion object
+
+function myFunction(propName, propValue) { // Change this line
+    //Only change code below this line
+
+    lion[propName] = propValue;
+    return lion;
+
+    // Only change code above this line
+}
+
+myFunction("roar", "roar-roar");
+module.exports = myFunction;
